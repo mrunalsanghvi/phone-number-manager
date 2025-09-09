f219609a8981f0b4fbc54abe75a10d1575b086f7
mrunalsanghvi <followuntamed@gmaill.com>
2025-09-09 13:13:37 +0530

    upddating validation and adding readme

--- internal/validation/custom.go
+++ internal/validation/custom.go
@@ -10,6 +10,10 @@ import (
 
 func ValidateE164Phone(phoneNumber string) (*phonenumbers.PhoneNumber, error) {
 	// Parse the E.164 number
+	if !isSpacingOkay(phoneNumber) {
+		log.Printf("Invalid phone number format: %s", phoneNumber)
+		return nil, fmt.Errorf("invalid phone number format")
+	}
 	var phoneRegex = regexp.MustCompile(`^\+?\d+( \d+){0,2}$`)
 	if !phoneRegex.MatchString(phoneNumber) {
 		log.Printf("Invalid phone number format: %s", phoneNumber)
@@ -21,4 +25,20 @@ func ValidateE164Phone(phoneNumber string) (*phonenumbers.PhoneNumber, error) {
 		return nil, err
 	}
 	return num, nil
+}
+
+func isSpacingOkay(phoneNumber string) bool {
+	spaceCount := 0
+	for i := 0; i < len(phoneNumber); i++ {
+		if phoneNumber[i] == ' ' {
+			spaceCount++
+		}
+	}
+	if spaceCount > 2 {
+		return false
+	}
+	if phoneNumber[4] == ' ' {
+		return false
+	}
+	return true
 }
